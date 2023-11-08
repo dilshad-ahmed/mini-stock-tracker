b5737a0ff27c6b50589be2ed665de956d018763c
dilshad-ahmed <dilshad7427@gmail.com>
2023-11-08 12:14:56 +0530

    fixed PORT typo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const connectMongoDb = require("./utility/db");
 const dotenv = require("dotenv");
 dotenv.config();
 
-app.listen(process.env.port, () => {
-  console.log(`server is running on port ${process.env.port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
   connectMongoDb();
 });
